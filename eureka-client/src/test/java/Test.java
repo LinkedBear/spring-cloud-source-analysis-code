54ef94019f739d6d66610963e791bcc674f925af
LinkedBear <742532002@qq.com>
2019-12-16 16:41:04 +0800

    初始导入

---
+++ eureka-client/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Title Test
+ * @Author LinkedBear
+ * @Time 2019年12月11日 14:54
+ */
+public class Test {
+    public static void main(String[] args) throws Exception {
+        System.out.println(System.currentTimeMillis());
+    }
+}
